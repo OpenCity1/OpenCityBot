fe4b813c121cdc2be3583926638fec63580de6a7
sairam4123 <61322406+sairam4123@users.noreply.github.com>
2020-05-07 22:46:04 +0530

    Development (#15)
    * Help Update 1.5.0
    
    * Prefix Update 1.6.0
    
    * SubCommands Update 1.7.0
    
    * Group Update 1.8.0
    
    * Path Update 1.8.1

--- bot.py
+++ bot.py
@@ -2,14 +2,17 @@ import datetime
 import json
 import logging
 import os
+import random
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 PREFIX = os.getenv('DEFAULT_PREFIX')
+USERS_FILE = os.getenv('USERS_FILE')
+PREFIX_FILE = os.getenv('PREFIX_JSON')
 
 
 def get_prefix(bot, message):
@@ -32,7 +35,9 @@ def get_prefix(bot, message):
 
 bot = commands.Bot(command_prefix=get_prefix)
 bot.start_time = datetime.datetime.utcnow()
-bot.prefix_default = PREFIX
+bot.prefix_default = PREFIX.split(" ")
+bot.users_json = USERS_FILE
+bot.prefix_json = PREFIX_FILE
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -62,7 +67,7 @@ async def on_command_error(ctx: commands.Context, error: commands.CommandError):
 
 @bot.event
 async def on_ready():
-	await bot.change_presence(activity=discord.Game(name="OpenCity • Type !help to get started"))
+	await bot.change_presence(activity=discord.Game(name=f"OpenCity • Type {random.choice(bot.prefix_default)}help to get started"))
 	# guild = discord.utils.get(client.guildTry .helps, id=GUILD_ID)
 	roles_needed = ["Muted Members", "Banned Members", "Kicked Members"]
 	for guild_index, guild in enumerate(bot.guilds):
@@ -88,6 +93,13 @@ for filename in os.listdir('./cogs'):
 		bot.load_extension(f'cogs.{filename[:-3]}')
 
 
+@tasks.loop(hours=1)
+async def my_presence_per_day():
+	await bot.wait_until_ready()
+	await bot.change_presence(activity=discord.Game(name=f"OpenCity • Type {random.choice(bot.prefix_default)}help to get started"))
+	print("changed")
+
+
 @bot.command()
 @commands.is_owner()
 async def reload_all_extensions(ctx):
@@ -97,5 +109,7 @@ async def reload_all_extensions(ctx):
 			bot.load_extension(f'cogs.{filename1[:-3]}')
 	await ctx.send("Reloaded all extensions!")
 
+
+my_presence_per_day.start()
 
 bot.run(TOKEN)
