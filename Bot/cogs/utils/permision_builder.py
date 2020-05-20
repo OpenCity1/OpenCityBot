c67c408d5c0fd23d5f6a66b62a3d407efbc20dc3
sairam4123 <opencity@info.github.com>
2020-05-20 12:07:06 +0530

    Reindent all files to 4 spaces from Tabs Update (cont.) 1.24.2

--- Bot/cogs/utils/permision_builder.py
+++ Bot/cogs/utils/permision_builder.py
@@ -5,19 +5,19 @@ perms = [[68608, 68608, 1117184, 3214336, 3230720, 3230720, 36785152, 36785152, 
          [1861312449, 1861312449, 1861320641, 1861451713, 1861713857, 1861713857, 1861713859, 1861713863, 1878491079, 2146926535, 2146926535]
          ]
 list2 = [
-	list(reversed(list3)) for list3 in perms
+    list(reversed(list3)) for list3 in perms
 ]
 
 
 def permission_builder(perms_list):
-	discord_perms_list_1 = []
-	for i in range(len(perms_list)):
-		perms_list_1 = perms_list[i]
-		discord_perms_list = [discord.Permissions(value) for value in perms_list_1]
-		discord_perms_list_1.append(discord_perms_list)
+    discord_perms_list_1 = []
+    for i in range(len(perms_list)):
+        perms_list_1 = perms_list[i]
+        discord_perms_list = [discord.Permissions(value) for value in perms_list_1]
+        discord_perms_list_1.append(discord_perms_list)
 
-	return discord_perms_list_1
+    return discord_perms_list_1
 
 
 if __name__ == '__main__':
-	print(len(permission_builder(list2)[0]))
+    print(len(permission_builder(list2)[0]))
